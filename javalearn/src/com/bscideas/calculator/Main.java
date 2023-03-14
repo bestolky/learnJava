d7854936a68b6d81c6db6d1b58aa17cc92b95f00
bestolky <maxim.fadeev98@gmail.com>
2023-03-14 14:53:47 +0300

    Validation Calculator

--- javalearn/src/com/bscideas/calculator/Main.java
+++ javalearn/src/com/bscideas/calculator/Main.java
@@ -7,34 +7,58 @@ public class Main {
 
 	public static void main(String[] args) {
 		
+		
+		Calculator perem = new Calculator();
+		int number = 0;
+		int number1 = 0;
+		int choice = 0;
+		
 		sec = new Scanner(System.in);
 		  
 	    System.out.println("Введите число A:");
+	    
+	    try {
+	    	number = sec.nextInt();
+	    } catch (Exception e) {
+	    	System.out.println("Введено не число");
+	    	System.exit(1);
+	    }
 	       
-	    int number = sec.nextInt();
-	       
+   
 	    System.out.println("Введите число B:");
 	       
-	    int number1 = sec.nextInt();
-	       
+	    try {
+	    	number1 = sec.nextInt();
+	    } catch (Exception e) {
+	    	System.out.println("Введено не число");
+	    	System.exit(1);
+	    }
+	    //int number1 = sec.nextInt();
+	      
 	    System.out.println("Если вам нужно умножение, введите - 1, деление - 2, сложение - 3, вычитание - 4");
 	       
-	    int choice = sec.nextInt();
+
+	    try {
+	    	choice = sec.nextInt();
+	    	if(choice == 1) {
+		    	   perem.multiplication(number, number1);
+		       } else if (choice == 2) {
+		    	   perem.division(number, number1);
+		       } else if (choice == 3) {
+		    	   perem.addition(number, number1);
+		       } else if (choice == 4) {
+		    	   perem.subtraction(number, number1);
+		       }
+	    } catch (Exception e) {
+	    	System.out.println("Вы выбрали некорректное значение");
+	    	System.exit(1);
+	    }
 	    
-	    Calculator perem = new Calculator();
 	    
-	    if(choice == 1) {
-	    	   perem.multiplication(number, number1);
-	       } else if (choice == 2) {
-	    	   perem.division(number, number1);
-	       } else if (choice == 3) {
-	    	   perem.addition(number, number1);
-	       } else if (choice == 4) {
-	    	   perem.subtraction(number, number1);
-	       } else {
-	       System.out.println("Вы выбрали некорректное значение");
-	       }
-	       
+	  
+	    
+	   
+     
 	    System.out.println("Ваш результат: " + perem.sum);
 	  
 	}
