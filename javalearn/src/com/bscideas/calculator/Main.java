a0954c40e285bbe589087bdadbe1cf7d03ef4857
bestolky <maxim.fadeev98@gmail.com>
2023-03-26 15:58:32 +0300

    refactor

--- javalearn/src/com/bscideas/calculator/Main.java
+++ javalearn/src/com/bscideas/calculator/Main.java
@@ -3,78 +3,12 @@ import java.util.Scanner;
 
 public class Main {
 
-	private static Scanner sec;
-
 	public static void main(String[] args) {
 		
-		Calculator perem = new Calculator();
+		Calculator calc = new Calculator();
 		
-		int number = 0;
+		calc.inputValue();
 		
-		int number1 = 0;
-		
-		int choice = 0;
-		
-		sec = new Scanner(System.in);
-		  
-	    System.out.println("Введите число A:");
-	    
-	    try {
-	    	number = sec.nextInt();
-	    	
-	    } catch (Exception e) {
-	    	
-	    	System.out.println("Введено не число/не целое число");
-	    	
-	    	System.exit(1);
-	    }    
-   
-	    System.out.println("Введите число B:");
-	       
-	    try {
-	    	
-	    	number1 = sec.nextInt();
-	    	
-	    } catch (Exception e) {
-	    	
-	    	System.out.println("Введено не число/не целое число");
-	    	
-	    	System.exit(1);
-	    }
-	      
-	    System.out.println("Если вам нужно умножение, введите - 1, деление - 2, сложение - 3, вычитание - 4");
-	       
-
-	    try {
-	    	
-	    	choice = sec.nextInt();
-	    	
-	    	if(choice == 1) {
-	    		
-		    	   perem.multiplication(number, number1);
-		    	   
-		       } else if (choice == 2) {
-		    	   
-		    	   perem.division(number, number1);
-		    	   
-		       } else if (choice == 3) {
-		    	   
-		    	   perem.addition(number, number1);
-		    	   
-		       } else if (choice == 4) {
-		    	   
-		    	   perem.subtraction(number, number1);
-		    	   
-		       }
-	    } catch (Exception e) {
-	    	
-	    	System.out.println("Вы выбрали некорректное значение");
-	    	
-	    	System.exit(1);
-	    }
-
-	    System.out.println("Ваш результат: " + perem.sum);
-	  
 	}
 
 }
